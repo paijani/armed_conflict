11908276c76961e12811cd465b109ddf708154af
Paijani <paijani.sheth@mail.utoronto.ca>
2023-10-05 18:40:42 -0400

    adding eda

--- scripts/exploratory_analysis.R
+++ scripts/exploratory_analysis.R
@@ -1,2 +1,3 @@
 #exploratory analysis
 finaldata <- read.csv(here("original", "covariates.csv"), header = TRUE)
+#she did a ggplot of maternal mor over year labelling armed conflict (0,1) in legend. she did this for each outcome. 
