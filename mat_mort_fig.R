7688e12c1383404a44db30d269ef241f69ba5378
Paijani <paijani.sheth@mail.utoronto.ca>
2023-10-17 16:23:19 -0400

    finished making fig

--- mat_mort_fig.R
+++ mat_mort_fig.R
@@ -5,11 +5,11 @@ library(tidyr)
 library(dplyr)
 
 #load final data 
-final_data <- read.csv(here("clean", "finaldata.csv"))
+final_data <- read.csv(here("clean", "final_data.csv"))
 
 #make df for fig
 fig_data <- final_data %>%
-  dplyr::select(ISO, year, Maternal.Mortality.rate) %>%
+  dplyr::select(country_name, ISO, year, Maternal.Mortality.rate) %>%
   filter(year < 2018) %>%
   group_by(ISO) %>%
   #create new variable that gives the difference between each year and baseline year
@@ -28,38 +28,19 @@ fig_data <- final_data %>%
 length(unique(fig_data$ISO)) #output says 13 countries
   
 #make fig
-fig1 <- fig_data |>
+fig1 <- fig_data %>%
   ggplot(aes(x = year, y = Maternal.Mortality.rate, group = ISO)) +
-  geom_line(aes(color = ISO), alpha = 1, linewidth = 1) +
+  geom_line(aes(color = country_name), alpha = 1, linewidth = 1) +
   xlim(c(2000,2017)) +
-  # use log 10 sclae for y axis
+  # use log 10 scale for y axis
   scale_y_continuous(trans='log10') + 
-  labs(y = "Maternal mortality (log 10 scale)", x = "Year", color = "Country", title = "Trend in maternal mortality for countries that had an increase from 2000 to 2017") + 
+  labs(y = "Maternal Mortality (log 10 scale)", x = "Year", color = "Country", title = "Trends in maternal mortality for countries that had an increase from 2000 to 2017") + 
   # use black and white theme and increase the size of labels
   theme_bw(base_size = 12)
 
 
-  #include only the countries whose matmor increased from 2000 to 2017
-  dplyr::filter(incmatmor == 1)
-  #resort data by country and decreasing year
-  arrange(ISO, year) %>%
-  ungroup() %>%
+fig1
+
+#save plot as png
+ggsave(fig1, file = here("figures", "fig1_matmor.png"), width = 8, height = 5)
   
-  # only select variables needed to create the figure
-  :select(country_name, ISO, year, matmor) |>
-  # select years 2000 to 2017
-  dplyr::filter(year < 2018) |>
-  # gropu by country
-  group_by(ISO) |>
-  # create a new variable diffmatmor that takes the difference between each year and baseline year 
-  mutate(diffmatmor = matmor - matmor[1L]) |>
-  # sort the data by country and decreasing year
-  arrange(ISO, desc(year)) |>
-  # if the first difference is greater than 0 (if year 2017 is greater than baseline, then incmatmor = 1)
-  mutate(incmatmor = ifelse(diffmatmor[1L] > 0 , 1, 0)) |>
-  # re-sort data by country and increasing year
-  arrange(ISO, year) |>
-  ungroup() |>
-  # only include countries that had an increase in maternal mortality from 2000 to 2017
-  dplyr::filter(incmatmor == 1)
-length(unique(forfigure$ISO))
