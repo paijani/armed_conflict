3ba1a1020199d607a3b38c5b62945438bce30219
Paijani <paijani.sheth@mail.utoronto.ca>
2023-11-10 19:59:29 -0500

    Ran SimDesign

--- scripts/simDesign.R
+++ scripts/simDesign.R
@@ -1,15 +1,25 @@
-## set simulation parameters
-n <- 100 # sample size
-pz <- 0.2 # probability of Z = 1
-alpha0 <- 0 # logit probability of x = 1 in non-smokers (z = 0)
-alpha1 <- 1 # log odds ratio of x = 1 in smokers (z = 1) vs non-smokers
-beta0 <- -3 # logit probability of y = 1 in non-coffee drinkers (x = 0) and non-smokers (z = 0)
-beta1 <- 0
-beta2 <- 2
-simnum <- 1000 # number of iterations to run in the simulation
-# empty vectors to store the values
-unadj.p <- adj.p <- rep(NA, simnum)
-for(s in 1:simnum){
+#load libraries
+
+library(SimDesign)
+
+
+#Design
+Design <- createDesign(n = c(50, 500),
+                      pz = c(0.2, 0.8),
+                      alpha0 = c(-1, 0, 1),
+                      alpha1 = c(0, 0.5, 1))
+head(Design)
+
+#Generate 
+
+Generate <- function (condition, fixed_objects = NULL ) {
+  n <- condition$n
+  pz <- condition$pz
+  alpha0 <- condition$alpha0
+  alpha1 <- condition$alpha1
+  beta0 = -3
+  beta1 = 0
+  beta2 = 2
   ## generate confounder Z from a binomial distribution
   z <- rbinom(n, size = 1, prob = pz)
   ## compute probability of observing X = 1 from the inverse logit function
@@ -21,18 +31,63 @@ for(s in 1:simnum){
   y <- rbinom(n, size = 1, prob = py)
   ## combine three random variables into a data frame
   dat <- data.frame(lung = y, coffee = x, smoke = z)
-  ## fit unadjusted logistic regression model
-  unadj.mod <- glm(lung ~ coffee, data = dat, family = "binomial")
-  unadj.coef <- summary(unadj.mod)$coef
-  ## fit adjusted logistic regression model
-  adj.mod <- glm(lung ~ coffee + smoke, data = dat, family = "binomial")
-  adj.coef <- summary(adj.mod)$coef
-  ## save p-values for coffee from both models in a vector
-  unadj.p[s] <- unadj.coef[2,4]
-  adj.p[s] <- adj.coef[2,4]
-  ## show simulation progress
-  print(s)
+  dat
 }
-## calculate the type 1 error rate from each model
-mean(ifelse(unadj.p < 0.05, 1, 0))
-mean(ifelse(adj.p < 0.05, 1, 0))
+
+#Analyze
+## fit unadjusted logistic regression model
+Analyse.unadj <- function(condition, dat, fixed_objects = NULL) {
+  mod <- glm(lung ~ coffee, data = dat, family = "binomial")
+  beta <- coef(mod)[2]
+  pval <- summary(mod)$coef[2,4]
+  ret <- c(unadj = unname(pval))
+  ret
+}
+
+## fit adjusted logistic regression model
+Analyse.adj <- function(condition, dat, fixed_objects = NULL) {
+  mod <- glm(lung ~ coffee + smoke, data = dat, family = "binomial")
+  beta <- coef(mod)[2]
+  pval <- summary(mod)$coef[2,4]
+  ret <- c(unadj = unname(pval))
+  ret
+}
+
+#Summarise
+Summarise <- function(condition, results, fixed_objects = NULL){
+  ret <- EDR(results, alpha = 0.05)
+  ret
+}
+
+res <- runSimulation(Design, replications = 10,
+                         parallel = TRUE, 
+                         generate = Generate,
+                         analyse = Analyse.unadj, Analyse,adj,
+                         summarise = Summarise)
+
+head(results)
+
+# Create a plot that summarizes the results
+
+reslong <- results %>%
+  pivot_longer(cols = c("unadj.p", "adj.p"),
+               names_to = "model",
+               values_to = "edr")
+
+reslong$n <- factor(reslong$n, levels = c(50,100,500),
+                              ordered = TRUE, labels=c("n == 50", "n == 100", "n == 500"))
+reslong$alpha0 <- factor(reslong$alpha0, levels = c(-1,0,1),
+                         ordered = TRUE, labels=c("alpha[0] == -1", "alpha[0] == 0", "alpha[0] == 1"))
+
+
+ggplot(reslong, aes(x = alpha1, y = edr, color = model, 
+                    linetype = as.factor(pz), shape = as.factor(pz), 
+                    group = interaction(model, pz))) +
+  geom_point(size = 2) + 
+  geom_line(linewidth = 1) + 
+  scale_color_discrete(labels=c('Adjusted', 'Unadjusted')) + 
+  facet_grid(n ~ alpha0, labeller = label_parsed) + 
+  theme_bw(base_size = 12) + 
+  geom_hline(yintercept = 0.05, color = "darkgray", linetype = "longdash") + 
+  labs(y = "Type I error", x = bquote(alpha[1]~values), color = "Model", shape = "Pr(Z=1)", linetype = "Pr(Z=1)", 
+       title = "Empirical type I error rates for adjusted and unadjusted models \nfrom each simulation scenario (gray dashed horizontal line at 0.05) ")
